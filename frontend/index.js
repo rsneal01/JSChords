80defbb61fd9bcd0a22c628b68750a3c6c9bc254
rsneal01 <rsneal01@gmail.com>
2021-11-28 10:29:35 -0800

    created fetch functions in index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,1 +1,20 @@
 const app = new App()
+
+function fetchArtists() {
+    return fetch("http://127.0.0.1:3000")
+      .then(resp => resp.json())
+      .then(json => renderArtists(json))
+  }
+
+  function renderArtists(artists) {
+    const main = document.querySelector('main')
+    artists.forEach(artist => {
+      const h2 = document.createElement('h2')
+      h2.innerHTML = artist.name
+      main.appendChild(h2)
+    })
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    fetchArtists()
+  })
