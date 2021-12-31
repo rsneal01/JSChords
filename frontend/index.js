28733eb866ec91c950b9a063dab3106c28520e3e
rsneal01 <rsneal01@gmail.com>
2021-12-31 10:19:58 -0800

    modified index.js, removed comments in attempt to solve render(artist) error

--- frontend/index.js
+++ frontend/index.js
@@ -9,25 +9,16 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 function getArtists() {
-  // const artistsContainer = document.querySelector('#artists-container')
     fetch(endPoint)
       .then(resp => {
       return resp.json()
     })
     .then(artist => {
       artist.data.forEach(artist => {
-          // check how data is nested
-          
+        debugger
           let newArtist = new Artist(artist, artist.attributes)
           render(artist)
       })
-      // .then(artists => {
-      //   artists.data.forEach(artist => {
-      //     const h2 = document.createElement('h2')
-      //     h2.innerText = artist.attributes.name
-      //     artistsContainer.appendChild(h2)
-
-      //   })
 
   })
 }
@@ -38,21 +29,12 @@ function render(artist) {
   h2.innerText = artist.attributes.name
   artistsContainer.appendChild(h2)
 
-  // const artistMarkup =
-  // <div data-id=${artist.id}>
-  // <h3>${artist.attributes.name}</h3>
-  // <button data-id=${artist.id}>edit</button>
-  // </div>
-  // <br></br>;
-
-  // document.querySelector('#artist-container').innerHTML += artistMarkup  
 }
 
 function createFormHandler(e) {
   e.preventDefault()
   const nameInput = document.querySelector('#input-name').value
-  // const foreignKeyId = parseInt(document.querySelector('#artists').value)
-  // query selector for grabbing foreign key
+  
   postFetch(nameInput)
 }
 
@@ -73,20 +55,3 @@ function postFetch(name) {
   })
 }
   
-  // function renderArtists(artists) {
-  //   const main = document.querySelector('main')
-  //   const h2 = document.createElement('h2')
-  //   h2.innerText = artists
-  //   main.appendChild(h2)
-  //   artists.forEach(artist => {
-  //     const h2 = document.createElement('h2')
-  //     h2.innerText = artist.name
-  //     main.appendChild(h2)
-  //   })
-  // }
-
-  
-
-  
-
-//   NEED EVENT LISTENER!!
