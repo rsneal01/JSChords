54d094016bd778ef07f820bb6f5b504155f9c3f1
rsneal01 <rsneal01@gmail.com>
2022-01-13 13:28:09 -0800

    modified index.js to getSongs instead of getArtists

--- frontend/index.js
+++ frontend/index.js
@@ -1,73 +1,74 @@
 const endPoint = "http://127.0.0.1:3000"
 
 document.addEventListener('DOMContentLoaded', function() {
-  getArtists()
+  getSongs()
 
-  const createArtistForm = document.querySelector('#create-artist-form')
+  // const createArtistForm = document.querySelector('#create-artist-form')
 
-  createArtistForm.addEventListener("submit", (e) => createFormHandler(e))
+  // createArtistForm.addEventListener("submit", (e) => createFormHandler(e))
 
   const createSongForm = document.querySelector('#create-song-form')
 
   createSongForm.addEventListener("submit", (e) => createSongFormHandler(e))
 })
 
-function getArtists() {
-    fetch(endPoint+`/artists`)
+function getSongs() {
+    fetch(endPoint+`/songs`)
       .then(resp => {
       return resp.json()
     })
-    .then(artist => {
-      artist.data.forEach(artist => {
+    .then(song => {
+      song.data.forEach(song => {
         
-          let newArtist = new Artist(artist, artist.attributes)
-          render(artist)
+          let newArtist = new Song(song, song.attributes)
+          render(song)
       })
 
   })
 }
 
-function render(artist) {
+function render(song) {
 
   const artistsContainer = document.querySelector('#artists-container')
   const h2 = document.createElement('h2')
-  const h3 = document.createElement('h3')
-  h2.innerText = artist.attributes.name
-  h3.innerText = artist.relationships.songs.data
+  const h4 = document.createElement('h4')
+  h2.innerText = song.attributes.name
+  h4.innerText = song.attributes.artist.name
   artistsContainer.appendChild(h2)
-  artistsContainer.appendChild(h3)
+  artistsContainer.appendChild(h4)
 }
 
-function createFormHandler(e) {
-  e.preventDefault()
-  const nameInput = document.querySelector('#input-name').value
+// function createFormHandler(e) {
+//   e.preventDefault()
+//   const nameInput = document.querySelector('#input-name').value
   
-  postFetch(nameInput)
-}
+//   postFetch(nameInput)
+// }
 
 
 
-function postFetch(name) {
-  const bodyData = {name}
-  fetch(endPoint+`/artists`, {
-    method: 'POST', 
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(bodyData)
-  })
-  .then(resp => {
-    return resp.json()
-  })
-  .then(artist => {
-    // artist is not being returned as json? no 'data' property
-    console.log(artist);
-    const artistsContainer = document.querySelector('#artists-container')
-    const h2 = document.createElement('h2')
-    h2.innerText = artist.name
-    artistsContainer.appendChild(h2)
-    // const artistData = artist.data
-    // render(artistData)
-  })
-}
+// function postFetch(name) {
+//   const bodyData = {name}
+//   fetch(endPoint+`/artists`, {
+//     method: 'POST', 
+//     headers: {'Content-Type': 'application/json'},
+//     body: JSON.stringify(bodyData)
+//   })
+//   .then(resp => {
+//     return resp.json()
+//   })
+//   .then(artist => {
+//     // artist is not being returned as json? no 'data' property
+//     console.log(artist);
+//     const artistsContainer = document.querySelector('#artists-container')
+//     const h2 = document.createElement('h2')
+//     h2.innerText = artist.name
+//     artistsContainer.appendChild(h2)
+//     // const artistData = artist.data
+//     // render(artistData)
+//   })
+// }
+
 
 function createSongFormHandler(e) {
   e.preventDefault()
@@ -78,7 +79,7 @@ function createSongFormHandler(e) {
 
 function songPostFetch(name) {
   const bodyData = {name}
-  fetch(endPoint+`/artists`, {
+  fetch(endPoint+`/songs`, {
     method: 'POST', 
     headers: {'Content-Type': 'application/json'},
     body: JSON.stringify(bodyData)
